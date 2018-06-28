c13feda6396a45b2603fbab6bc2fccedce1c1434
julian21olarte <julian.olarte@rokk3rlabs.com>
2018-06-28 18:59:52 -0500

    fix server

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(cors());
  
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
  
-app.use(express.static('www'));
+app.use(express.static(path.resolve(__dirname, "www")));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/www/index.html'));
 });
